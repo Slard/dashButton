dcc990efce458c3b9f31d3de66d0ad772a52e939
Slard <wiltshire.slade@gmail.com>
2018-11-26 06:48:08 -0500

    what i have so far with the api. Not sure if it'll include all the packages that i downloaded.

---
+++ Scripts/flashAPI.py
@@ -0,0 +1,42 @@
+''' LOOKUP EXAMPLE
+from amazon.api import AmazonAPI
+>>> amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
+>>> product = amazon.lookup(ItemId='B00EOE0WKQ')
+>>> product.title
+'Amazon Fire Phone, 32GB (AT&T)'
+>>> product.price_and_currency
+(199.0, 'USD')
+>>> product.ean
+'0848719035209'
+>>> product.large_image_url
+'http://ecx.images-amazon.com/images/I/51BrZzpkWrL.jpg'
+>>> product.get_attribute('Publisher')
+'Amazon'
+>>> product.get_attributes(['ItemDimensions.Width', 'ItemDimensions.Height'])
+{'ItemDimensions.Width': '262', 'ItemDimensions.Height': '35'}
+'''
+
+
+''' CREATE AND MANIPULATE SHOPPPING bCARTS EXAMPLE
+from amazon.api import AmazonAPI
+>>> amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
+>>> product = amazon.lookup(ItemId="B0016J8AOC")
+>>> item = {'offer_id': product.offer_id, 'quantity': 1}
+>>> cart = amazon.cart_create(item)
+>>> fetched_cart = amazon.cart_get(cart.cart_id, cart.hmac)
+>>> another_product = amazon.lookup(ItemId='0312098286')
+>>> another_item = {'offer_id': another_product.offer_id, 'quantity': 1}
+>>> another_cart = amazon.cart_add(another_item, cart.cart_id, cart.hmac)
+>>> cart_item_id = None
+>>> for item in cart:
+>>>     cart_item_id = item.cart_item_id
+>>> modify_item = {'cart_item_id': cart_item_id, 'quantity': 3}
+>>> modified_cart = amazon.cart_modify(item, cart.cart_id, cart.hmac)
+>>> cleared_cart = amazon.cart_clear(cart.cart_id, cart.hmac)
+'''
+from C:\Users\Slade\Downloads\python-amazon-simple-product-api-2.2.11\amazon\api.py import AmazonAPI
+
+amazon = AmazonAPI(AKIAJXA5GIJNO3U2A65, aDQYVdQ7ZMCBA2m40XoFYcOg+bz9Yrz2edNgIe8c)
+product = amazon.lookup(ItemId="B0016J8AOC")
+
+print(product.title())
